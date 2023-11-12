bc85edd16c2dd28a08c55a4ccea046ce78d18e2c
Joshua Hewitt <sposh@sposh.net>
2023-11-12 17:02:39 +0100

    feat(test): jest tests for basedto

--- src/BaseDto.js
+++ src/BaseDto.js
@@ -11,7 +11,17 @@ export default class BaseDto {
     }
 
     get _params() {
-        return [...this.#params]; // ensure immutability
+        return this.toInmutable(this.#params);
+    }
+
+    // TODO JSDoc
+    toInmutable(param) {
+        return Object.freeze(param);
+    }
+
+    // TODO JSDoc
+    toShallowCopy(param) { // TODO Handle other weird types like iterators
+        return param instanceof Array ? [...param] : param instanceof Object ? Object.assign({}, param) : param;
     }
 
     toString() {
